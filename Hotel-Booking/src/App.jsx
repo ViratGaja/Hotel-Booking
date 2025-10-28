19d14697d220aef7883c109dcf0f6add37fa660e
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2025-10-28 19:31:42 +0530

    detailspages_done

--- Hotel-Booking/src/App.jsx
+++ Hotel-Booking/src/App.jsx
@@ -4,6 +4,7 @@ import { useLocation, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Footer from './Components/Footer'
 import  AllRooms  from './Pages/AllRooms'
+import RoomDetails from './Pages/RoomDetails'
 const App = () => {
   const isOwnerPath=useLocation().pathname.includes("owner")
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/rooms' element={<AllRooms/>} />
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
         </Routes>
       </div>
       <Footer/>
